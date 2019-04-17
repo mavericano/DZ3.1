b62e021aa1f0ce570d62d83d2a3a02e3ee9f22a1
alexbor2015 <aborisenko2015@gmail.com>
2019-04-17 19:02:57 +0300

    alpha 1.1

--- src/lesson4/Dustbin.java
+++ src/lesson4/Dustbin.java
@@ -1,4 +1,11 @@
 package lesson4;
 
+import java.util.Scanner;
+
 public class Dustbin {
+    public static void main(String[] args) {
+        System.out.println("Please, type your command (insert money  [F] or make a foto [M]");
+        Scanner in = new Scanner(System.in);
+        String command = in.nextLine();
+    }
 }
